045203d963bbb1a0b0ab3bffd931567dcc1c08b8
Stanislav Markovskyi <smarkovskyi@zeuselectronics.eu>
2016-01-22 13:47:01 +0200

    Doc corrections

--- src/Nfilin/Libs/Yii/Controller.php
+++ src/Nfilin/Libs/Yii/Controller.php
@@ -32,9 +32,9 @@ abstract class Controller extends YiiController
      */
     public function beforeAction($action)
     {
-        if (!parent::beforeAction((array)$action)) {
+        /*if (!parent::beforeAction((array)$action)) {
             return false;
-        }
+        }*/
         $this->start_timemark = microtime(true);
 
         $request = Yii::$app->request;
