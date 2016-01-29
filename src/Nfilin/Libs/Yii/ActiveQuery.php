f861c262afcd437e1ae6aa7378673dd807302ba0
Stanislav Markovskyi <smarkovskyi@zeuselectronics.eu>
2016-01-29 13:43:29 +0200

    Batch Call for ActiveQuery

--- src/Nfilin/Libs/Yii/ActiveQuery.php
+++ src/Nfilin/Libs/Yii/ActiveQuery.php
@@ -9,48 +9,57 @@ use yii\db\Connection;
 /**
  * @inheritdoc
  */
-class ActiveQuery extends YiiActiveQuery implements ActiveQueryInterface{
+class ActiveQuery extends YiiActiveQuery implements ActiveQueryInterface
+{
+    protected $batch_calls = [];
 
-	/**
-	 * @param array $rows
-	 * @return array|\yii\db\ActiveRecord[]
-	 */
-	public function populate($rows){
-		$listClass = call_user_func([$this->modelClass, 'listClass']);
-		$rows = parent::populate($rows);
-		$rows = new $listClass($rows);
-		return $rows;
-	}
+    /**
+     * @param array $rows
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function populate($rows)
+    {
+        $listClass = call_user_func([$this->modelClass, 'listClass']);
+        $rows = parent::populate($rows);
+        foreach ($rows as $row) {
+            foreach ($this->batch_calls as $call) {
+                call_user_func($call, $row);
+            }
+        }
+        $rows = new $listClass($rows);
+        return $rows;
+    }
 
-	/**
-	 * @param Connection $db
-	 * @return ActiveRecord|null
-	 */
-	public function one($db = null){
-		$row = parent::one($db);
-		return $row;
-	}
+    /**
+     * @param Connection $db
+     * @return ActiveRecord|null
+     */
+    public function one($db = null)
+    {
+        $row = parent::one($db);
+        return $row;
+    }
 
-	/**
-	 * @param \yii\db\QueryBuilder $builder
-	 * @return \yii\db\Query
-	 * @throws \yii\base\InvalidConfigException
-	 */
-	public function prepare($builder)    {
+    /**
+     * @param \yii\db\QueryBuilder $builder
+     * @return \yii\db\Query
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function prepare($builder)
+    {
         /** @var ActiveRecord $modelClass */
         $modelClass = $this->modelClass;
         $schema = $modelClass::getTableSchema();
-        if (empty($this->select))   {
-	            $this->select('*');
-	            foreach ($schema->columns as $column)   {
-	                if (ActiveRecord::isSpatial($column)) {
-	                    $field = $column->name;
-	                    $this->addSelect(["AsText($field) AS $field"]);
-	                }
-	            }
-        }
-        else    {        	
-            foreach ($this->select as $column => $field)   {
+        if (empty($this->select)) {
+            $this->select('*');
+            foreach ($schema->columns as $column) {
+                if (ActiveRecord::isSpatial($column)) {
+                    $field = $column->name;
+                    $this->addSelect(["AsText($field) AS $field"]);
+                }
+            }
+        } else {
+            foreach ($this->select as $column => $field) {
                 $column = $schema->getColumn(is_numeric($column) ? $field : $column);
                 if (ActiveRecord::isSpatial($column)) {
                     $this->addSelect(["AsText($field) AS $field"]);
@@ -58,5 +67,16 @@ class ActiveQuery extends YiiActiveQuery implements ActiveQueryInterface{
             }
         }
         return parent::prepare($builder);
+    }
+
+    /**
+     * @param string $name
+     * @param callable $func
+     * @return $this
+     */
+    public function addBatchCall($name, callable $func)
+    {
+        $this->batch_calls[$name] = $func;
+        return $this;
     }
 }
